d7d23c1e60b1ba4db9d4e6b79176668564033e4c
Caio Lima <caiololima2@gmail.com>
2025-05-27 22:44:17 -0300

    Adicionar arquivos de projeto.

---
+++ ClubeDaLeitura/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClubeDaLeitura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trabalho 02 - Clube da leitura");
+        }
+    }
+}
